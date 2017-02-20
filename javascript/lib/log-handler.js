8ef1aa379f87a030f0ef4fbbbd64812d302c47ae
samson84 <samson84@gmail.com>
2017-02-20 18:33:46 +0100

    internal variable naming imroved, rename log.warning to log.warn, namespace support added to logging also

--- javascript/lib/log-handler.js
+++ javascript/lib/log-handler.js
@@ -1,20 +1,23 @@
-// { emerg: 0, alert: 1, crit: 2, error: 3, warning: 4, notice: 5, info: 6, debug: 7 }
 'use strict';
+/**
+ * Common library provides a logger and debugger setup for all the modules.
+ */
 
-var winston = require('winston');
 var slice = Array.prototype.slice;
 
-exports.getLogger = function () {
-    var logger = new winston.Logger({
+exports.getLogger = function (namespace) {
+    var loggerEngine = require('winston');
+
+    var logger = new loggerEngine.Logger({
         transports: [
-            new (winston.transports.Console)()
+            new (loggerEngine.transports.Console)()
         ]
     });
-    logger.setLevels(winston.config.syslog.levels);
+    logger.setLevels(loggerEngine.config.syslog.levels);
     logger.level = process.env.NODE_LOG_LEVEL || 'info';
 
     var logFunction = function (level, args) {
-        logger.log.apply(logger, [level].concat(slice.call(args, 0)));
+        logger.log.apply(logger, [level].concat(`[${namespace}]`).concat(slice.call(args, 0)));
     };
 
     console.log('LOGLEVEL: %s', logger.level);
@@ -24,7 +27,7 @@ exports.getLogger = function () {
         alert: function (message) { logFunction('alert', arguments); },
         crit: function (message) { logFunction('crit', arguments); },
         error: function (message) { logFunction('error', arguments); },
-        warning: function (message) { logFunction('warning', arguments); },
+        warn: function (message) { logFunction('warning', arguments); },
         notice: function (message) { logFunction('notice', arguments); },
         info: function (message) { logFunction('info', arguments); },
         debug: function (message) { logFunction('debug', arguments); }
@@ -32,10 +35,10 @@ exports.getLogger = function () {
 };
 
 exports.getDebugger = function (namespace) {
-    var debug = require('debug')(namespace);
+    var debuggerEngine = require('debug')(namespace);
 
     console.log('DEBUGGER NAMESPACE: %s', namespace);
     return function (message) {
-        debug.apply(null, slice.call(arguments));
+        debuggerEngine.apply(null, slice.call(arguments));
     }
 }
